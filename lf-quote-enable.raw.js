d586b9b33c06c8355cb009bfb9e7a5282c4db95b
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-02-17 15:05:58 -0800

    Raw JS code
    Nothing else except decompressed JS

--- lf-quote-enable.raw.js
+++ lf-quote-enable.raw.js
@@ -1,22 +1,3 @@
-// ==UserScript==
-// @name           LeakForums Quote Enable
-// @version        1.0.0
-// @description    Allow users to always quote people 
-// @downloadURL https://gitlab.com/notmike101/leakforums-quote-enable/raw/master/lf-quote-enable.user.js
-// @updateURL      https://gitlab.com/notmike101/leakforums-quote-enable/raw/master/lf-quote-enable.meta.js
-//
-// @author         Mike Orozco / IRDeNial
-// @namespace      leakforums/irdenial/customcss
-//
-// @include        *://leakforums.net/thread-*
-// @include        *://www.leakforums.net/thread-*
-// @include        *://*.leakforums.net/thread-*
-//
-// @grant          metadata
-// ==/UserScript==
-
-/*
-
 (function () {
     if (document.querySelector('.pq') !== null) {
         return false;
@@ -38,7 +19,3 @@
         thispost.querySelectorAll('td[align=right]')[0].appendChild(newAElement);
     }
 })();
-
-*/
-
-(function () { if (null !== document.querySelector(".pq")) return !1; for (var c = document.querySelectorAll('table[id^="post_"]'), b = 0; b < c.length; ++b) { var d = c[b], e = d.id.split("post_")[1], f = document.location.pathname.replace("/", "").split("&")[0].replace("thread-", ""), a = document.createElement("a"); a.setAttribute("href", "newreply.php?tid=" + f + "&pid=" + e); a.setAttribute("post", e); a.setAttribute("class", "pq"); a.innerText = "Quote"; d.querySelectorAll("td[align=right]")[0].appendChild(a);}})();
